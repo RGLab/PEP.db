da109967d5cb267b33f87cd304b1b74182340e00
SRenan <renan.sauteraud@gmail.com>
2013-06-28 15:15:57 -0700

    cleanup

--- inst/raw/mac239E543.R
+++ inst/raw/mac239E543.R
@@ -20,33 +20,13 @@ fix <- data.frame(start=131, end=145, query_sequence = "ASTTSTTASAKVDMV", polypr
 tab <- rbind(tab, fix)
 tab <- tab[order(tab$start),]
 
-#Get zScores
-tab <- cbind(tab, makeZpepMatrix(as.character(tab$query_sequence)))
-
-rd <- RangedData(ranges=IRanges(start= tab$start, end= tab$end, names= tab$query_sequence), space=tab$polyprotein, names=tab$query_sequence, clade="mac239", z1 = tab$z1, z2 = tab$z2, z3 = tab$z3, z4 =  tab$z4, z5 = tab$z5)
 
 
 tab2 <- read.table("smE543pos.tsv", header=TRUE)
 tab2 <- tab2[,c("start", "end", "query_sequence", "polyprotein")]
 tab2 <- tab2[order(tab2$start),]
-tab2 <- cbind(tab2, makeZpepMatrix(as.character(tab2$query_sequence)))
-rd2 <- RangedData(ranges=IRanges(start= tab2$start, end= tab2$end, names= tab2$query_sequence), space=tab2$polyprotein, names=tab2$query_sequence, clade="smE543", z1 = tab2$z1, z2 = tab2$z2, z3 = tab2$z3, z4 =  tab2$z4, z5 = tab2$z5) 
-
-#change clade info
-rdo <- rd[rownames(rd)%in%rownames(rd2),]
-rdo$clade <- "mac239,smE543"
-rd2 <- rd2[!(rownames(rd2) %in% rownames(rdo)),]
-rd <- rd[!(rownames(rd) %in% rownames(rdo)),]
-
-pep_m239smE543 <- rbind(rd,rd2,rdo)
-save(pep_m239smE543, file="pep_m239smE543.rda")
 
 
-##mannualy correct missaligned peptides
-#fixQS <- c("DQNSNRWKQQKKPEQ", "TTAKSTTSTTTTTVT", "VEDRDQNSNRWKQQK", "AETTTTAKSTTSTTT") #Matched with Vif Tat Vif 
-#fixStart <-c( , 131, , 127)
-#fixEnd <- c( , 145, , 141)
-#fix <- data.frame(start=131, end=145, query_sequence = "ASTTSTTASAKVDMV", polyprotein="gp160")
 
 #STRAT 2
 #1. Reconstruct sequences from the peptides
@@ -81,6 +61,7 @@ for(i in 2:len){
   if(i%%10==0)
     print(i)
 }
+alignmentFile <- system.file("extdata/alignmentsMuscleLANL_239_E543.fasta", package=PEP.db)
 
 #Get positions
 starts <- c()
@@ -91,22 +72,20 @@ ends <- starts+14
 
 #Get extended positions
 if(clade=="mac239"){
-  #refScale <- Pviz::readAlign("inst/extdata/alignments/MuscleLANL_239_E543.fasta", seqLine=4)[[1]]
-  refScale <- Pviz::readAlign("~/workspace/BioC/PEP.db/inst/extdata/alignments/MuscleLANL_239_E543.fasta", seqLine=4)[[1]]
+  refScale <- Pviz::readAlign(alignmentFile, seqLine=4)[[1]]
 else{
-  #refScale <- Pviz::readAlign("inst/extdata/alignments/MuscleLANL_239_E543.fasta", seqLine=6)[[1]]
-  refScale <- Pviz::readAlign("~/workspace/BioC/PEP.db/inst/extdata/alignments/MuscleLANL_239_E543.fasta", seqLine=6)[[1]]
+  refScale <- Pviz::readAlign(alignmentFile, seqLine=6)[[1]]
 }
 extStarts <- Pviz::coord2ext(starts, refScale)
 extEnds <- Pviz::coord2ext(ends, refScale)
 
 #Get LANLmac239 positions
-LANLscale <- Pviz::readAlign("inst/extdata/alignments/MuscleLANL_239_E543.fasta")[[1]]
+LANLscale <- Pviz::readAlign(alignmentFile)[[1]]
 LANLstarts <- LANLscale[extStarts]
 LANLends <- LANLscale[extEnds]
 
 #Get alignment for each peptide
-refSeq <- Pviz::readAlign("inst/extdata/alignments/MuscleLANL_239_E543.fasta")[[2]]
+refSeq <- Pviz::readAlign(alignmentFile)[[2]]
 aligned <- c()
 for(i in 1:len){
   aligned <- c( aligned, substr(refSeq, extStarts[i], extEnds[i]))
