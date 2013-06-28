01ddef6f2232e26eb7d3b52030c05d0ad9230adf
SRenan <renan.sauteraud@gmail.com>
2013-06-28 14:22:26 -0700

    Realigned pep_m239smE543

--- inst/raw/mac239E543.R
+++ inst/raw/mac239E543.R
@@ -47,3 +47,84 @@ save(pep_m239smE543, file="pep_m239smE543.rda")
 #fixStart <-c( , 131, , 127)
 #fixEnd <- c( , 145, , 141)
 #fix <- data.frame(start=131, end=145, query_sequence = "ASTTSTTASAKVDMV", polyprotein="gp160")
+
+#STRAT 2
+#1. Reconstruct sequences from the peptides
+#2. Align the sequence to LANL mac239
+#3. locate the peptides on the custom seq
+#4. translate into LANL mac239 coords
+
+clade <- "mac239"
+#clade <- "smE543"
+if(clade=="mac239"){
+  peps <- as.character(tab$query_sequence)
+  seq <- "MGCLGNQLLIAILLL"
+} else{
+  peps <- as.character(tab2$query_sequence)
+  seq <- "MGCLGNQLLIALLLV"
+}
+
+#Check that peptides do overlap
+len <- length(peps)
+for(i in 2:len){
+  res <- grep(substr(peps[i-1], nchar(peps[i-1])-2, nchar(peps[i])), peps[i])
+  pairwiseAlignment(peps[i-1], peps[i], type="overlap")
+  if(res!=1)
+    stop("no overlap")
+}
+
+for(i in 2:len){
+  res <- pairwiseAlignment(substr(seq, nchar(seq)-14, nchar(seq)), peps[i], type="overlap")
+  ovlp <- as.character(res@subject)
+  toAdd <- gsub(ovlp, "", peps[i])
+  seq <- paste0(seq, toAdd)
+  if(i%%10==0)
+    print(i)
+}
+
+#Get positions
+starts <- c()
+for(i in 1:len){
+  starts <- c(starts, regexpr(peps[i], seq)[[1]])
+}
+ends <- starts+14
+
+#Get extended positions
+if(clade=="mac239"){
+  #refScale <- Pviz::readAlign("inst/extdata/alignments/MuscleLANL_239_E543.fasta", seqLine=4)[[1]]
+  refScale <- Pviz::readAlign("~/workspace/BioC/PEP.db/inst/extdata/alignments/MuscleLANL_239_E543.fasta", seqLine=4)[[1]]
+else{
+  #refScale <- Pviz::readAlign("inst/extdata/alignments/MuscleLANL_239_E543.fasta", seqLine=6)[[1]]
+  refScale <- Pviz::readAlign("~/workspace/BioC/PEP.db/inst/extdata/alignments/MuscleLANL_239_E543.fasta", seqLine=6)[[1]]
+}
+extStarts <- Pviz::coord2ext(starts, refScale)
+extEnds <- Pviz::coord2ext(ends, refScale)
+
+#Get LANLmac239 positions
+LANLscale <- Pviz::readAlign("inst/extdata/alignments/MuscleLANL_239_E543.fasta")[[1]]
+LANLstarts <- LANLscale[extStarts]
+LANLends <- LANLscale[extEnds]
+
+#Get alignment for each peptide
+refSeq <- Pviz::readAlign("inst/extdata/alignments/MuscleLANL_239_E543.fasta")[[2]]
+aligned <- c()
+for(i in 1:len){
+  aligned <- c( aligned, substr(refSeq, extStarts[i], extEnds[i]))
+}
+zMat <- makeZpepMatrix(peps)
+if(clade=="mac239"){
+  rdm239 <- RangedData(ranges=IRanges(start=extStarts, end=extEnds, names=peps),space="gp160", aligned=aligned, clade=clade, zMat)
+} else{
+  rdE543 <- RangedData(ranges=IRanges(start=extStarts, end=extEnds, names=peps),space="gp160", aligned=aligned, clade=clade, zMat)
+}
+
+rdo <- rdm239[rownames(rdm239) %in% rownames(rdE543),]
+rdo$clade <- "mac239,E543"
+rd1 <- rdm239[!rownames(rdm239)%in%rownames(rdo),]
+rd2 <- rdE543[!rownames(rdE543)%in%rownames(rdo),]
+pep_m239smE543 <- rbind(rd1, rdo, rd2)
+save(pep_m239smE543, file="pep_m239smE543.rda")
+
+
+
+
